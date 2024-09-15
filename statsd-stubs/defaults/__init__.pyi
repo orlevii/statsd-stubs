0204b98f26eda3d11b350e556a2d12841e4a507b
Or Levi <orlevi128@gmail.com>
2024-09-15 17:53:30 +0000

    first commit

---
+++ statsd-stubs/defaults/__init__.pyi
@@ -0,0 +1,7 @@
+from _typeshed import Incomplete
+
+HOST: str
+PORT: int
+IPV6: bool
+PREFIX: Incomplete
+MAXUDPSIZE: int
---
+++ statsd-stubs/defaults/django.pyi
@@ -0,0 +1,10 @@
+from _typeshed import Incomplete
+from statsd import defaults as defaults
+from statsd.client import StatsClient as StatsClient
+
+statsd: Incomplete
+host: Incomplete
+port: Incomplete
+prefix: Incomplete
+maxudpsize: Incomplete
+ipv6: Incomplete
